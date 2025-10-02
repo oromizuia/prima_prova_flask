2261566d30199ff03a3acaae26ca4e16f8520276
oromizuia <147906155+oromizuia@users.noreply.github.com>
2025-10-02 14:12:10 +0200

    Update send_mail_flask.py

--- send_mail_flask.py
+++ send_mail_flask.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template_string
+from flask_cors import CORS
 import smtplib
 import ssl
 from email.message import EmailMessage
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 HTML_FORM = """
 <form method="post">
@@ -39,4 +42,5 @@ def send_mail():
     return render_template_string(HTML_FORM)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
